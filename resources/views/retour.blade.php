a5a268ace978d509e01cf6f840f88057ca1e5bdd
Ashvin02 <mariyathasashvin06@gmail.com>
2026-06-24 13:59:49 +0200

    Fix layout retour

--- resources/views/retour.blade.php
+++ resources/views/retour.blade.php
@@ -1,28 +1,18 @@
 @extends('template')
-
 @section('content')
-
 <h2>Retour d'un livre</h2>
-
 <p>Sélectionnez un livre à retourner :</p>
-
 <form method="POST" action="/retour">
-@csrf
-
-<select name="livre_id">
-
-@foreach($livres as $livre)
-
-<option value="{{ $livre->id }}">
-{{ $livre->titre }} - {{ $livre->auteur }}
-</option>
-
-@endforeach
-
-</select>
-
-<button type="submit" class="btn btn-primary">Retourner le livre</button>
-
+    @csrf
+    <div class="d-flex align-items-center gap-2">
+        <select name="livre_id" class="form-select" style="max-width: 350px;">
+            @foreach($livres as $livre)
+            <option value="{{ $livre->id }}">
+                {{ $livre->titre }} - {{ $livre->auteur }}
+            </option>
+            @endforeach
+        </select>
+        <button type="submit" class="btn btn-primary">Retourner le livre</button>
+    </div>
 </form>
-
 @endsection
